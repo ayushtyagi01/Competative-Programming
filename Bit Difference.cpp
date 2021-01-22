b7f9aac37b70ad6c0d8a66a22eebf43c4051a5e1
ayushtyagi01 <54800927+ayushtyagi01@users.noreply.github.com>
2021-01-22 22:50:18 +0530

    Bit Difference

---
+++ Bit Difference.cpp
@@ -0,0 +1,11 @@
+// count bits to be flipped to convert number A to B
+int countBitsFlip(int a, int b){
+    
+    int n=a^b;
+    int c=0;
+    while(n!=0){
+        n=n&(n-1);
+        c++;
+    }
+    return c;
+}
